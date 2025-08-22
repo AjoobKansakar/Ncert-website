d3cd0e0ab7b1dcad1ec65d321d2b4ce14ec4b0a3
AjoobKansakar <ajoob.sagar@gmail.com>
2025-08-22 16:18:23 +0545

    Header section completed

--- nepcert/src/component/Header.jsx
+++ nepcert/src/component/Header.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import NepcertLogo from '../assets/NepcertLogo';
+import NepcertLogo from '../assets/Nepcert Logo.png';
 
 function Header () {
     return (
         <header>
-            <div className="nav-bar">
-                <a href="#" className="logo">
-                    <img src={NepcertLogo} alt="Nepcert logo"/>
-                </a>
-                <nav>
-                    <a href="#">About us</a>
-                    <a href="#">Products</a>
-                    <a href="#">Services</a>
-                    <a href="#">Mission Statement</a>
+            <div className="web-wrapper">
+                <div className="nav-bar">
+                    <a href="#" className="logo">
+                        <img src={NepcertLogo} alt="Nepcert logo" id="nepcert-logo"/>
+                    </a>
+                    <nav className="Nav-items">
+                        <a href="#">About us</a>
+                        <a href="#">Products</a>
+                        <a href="#">Services</a>
+                        <a href="#">Mission Statement</a>
 
-                    <button className="contact-usbtn">Contact us</button>
-                </nav>
+                        <button className="contact-usbtn">Contact us</button>
+                    </nav>
+                </div>
             </div>
         </header>
     );
