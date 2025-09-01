11f81c0a01da1ba9b1baa3cca8da0f1d18b5d931
AjoobKansakar <ajoob.sagar@gmail.com>
2025-09-01 14:32:32 +0545

    adding services section navigation

--- nepcert/src/component/Header.jsx
+++ nepcert/src/component/Header.jsx
@@ -12,7 +12,7 @@ function Header () {
                     <nav className="Nav-items">
                         <a href="#AboutUs">About us</a>
                         <a href="#Products">Products</a>
-                        <a href="#">Services</a>
+                        <a href="#services">Services</a>
                         <a href="#">Mission Statement</a>
                         <button className="contact-usbtn">Contact us</button>
                     </nav>
