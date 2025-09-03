b28b11179559847ac74db23ba6948637120a2829
AjoobKansakar <ajoob.sagar@gmail.com>
2025-09-03 15:40:13 +0545

    Contact us Banner completed

--- nepcert/src/component/ContactUs/ContactUs.jsx
+++ nepcert/src/component/ContactUs/ContactUs.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './ContactUs.css';
+import ClientBannerImg from '../../assets/ContactBanner_img.jpg';
 
 function ContactUs () {
     return (
         <section className="Contact-us" id="Contact-us">
             <div className="contact-us-banner">
                 <h1> Contact Us </h1>
+                <img src={ClientBannerImg} alt="Client-banner-img"></img>
             </div>
         </section>
     )
