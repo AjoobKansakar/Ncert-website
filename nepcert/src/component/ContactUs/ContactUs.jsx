173ac802f29d6e129a55eb533277e09c082c8914
AjoobKansakar <ajoob.sagar@gmail.com>
2025-09-03 16:25:34 +0545

    Working on contact us section

--- nepcert/src/component/ContactUs/ContactUs.jsx
+++ nepcert/src/component/ContactUs/ContactUs.jsx
@@ -9,6 +9,19 @@ function ContactUs () {
                 <h1> Contact Us </h1>
                 <img src={ClientBannerImg} alt="Client-banner-img"></img>
             </div>
+
+            <div className="web-wrapper">
+                <div className="Contact-form-section">
+                    <div className="get-in-touch">
+                        <h2> Get in touch </h2>
+                    </div>
+
+                    <div className="contact-us-form">
+                        <p> Form </p>
+                    </div>
+                </div>
+            </div>
+
         </section>
     )
 }
