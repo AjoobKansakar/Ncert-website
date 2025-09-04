22cff46979a7947e8c546f1a34ec1e6bb447ac7b
AjoobKansakar <ajoob.sagar@gmail.com>
2025-09-04 14:05:14 +0545

    ContactUs form in progress

--- nepcert/src/component/ContactUs/ContactUs.jsx
+++ nepcert/src/component/ContactUs/ContactUs.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import './ContactUs.css';
 import ClientBannerImg from '../../assets/ContactBanner_img.jpg';
+// icons from fontawesome 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 function ContactUs () {
     return (
@@ -14,10 +19,18 @@ function ContactUs () {
                 <div className="Contact-form-section">
                     <div className="get-in-touch">
                         <h2> Get in touch </h2>
+                        <h3> <FontAwesomeIcon icon={ faLocationDot } style={{color: "#2A3B5F",}} /> Office </h3>
+                        <p> Kumaripati, Lalitpur </p>
+
+                        <h3> <FontAwesomeIcon icon={ faEnvelope } style={{color: "#2A3B5F",}} /> Email </h3>
+                        <p> info@nepcert.com </p>
+
+                        <h3> <FontAwesomeIcon icon={ faPhone } style={{color: "#2A3B5F",}} /> Contact </h3>
+                        <p> +977-9847395310 </p>
                     </div>
 
                     <div className="contact-us-form">
-                        <p> Form </p>
+                        <h2> Send us a message: </h2>
                     </div>
                 </div>
             </div>
