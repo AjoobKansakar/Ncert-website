b4dc0e6a55362f2b58287ffaa1a040403f1a050c
AjoobKansakar <ajoob.sagar@gmail.com>
2025-09-05 17:11:47 +0545

    Added Hover effect in the links including the icons

--- nepcert/src/component/Footer.jsx
+++ nepcert/src/component/Footer.jsx
@@ -7,40 +7,41 @@ function Footer () {
         <footer className="footer" id="footer">
             <div className="web-wrapper">
                 <h1> Nep <span>C</span>ert </h1>
-
+            <div className="all-links">
                 <div className="Quick-links">
                     <h2> Quick Links </h2>
                         <ul>
-                            <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> Home </li>
-                            <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> About us </li>
-                            <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> Products </li>
-                            <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> Services </li>
-                            <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> Contact us </li>
-                            <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> Mission Statements </li>
+                            <li> <FontAwesomeIcon icon={ faGreaterThan } /> Home </li>
+                            <li> <FontAwesomeIcon icon={ faGreaterThan } /> About us </li>
+                            <li> <FontAwesomeIcon icon={ faGreaterThan } /> Products </li>
+                            <li> <FontAwesomeIcon icon={ faGreaterThan } /> Services </li>
+                            <li> <FontAwesomeIcon icon={ faGreaterThan } /> Contact us </li>
+                            <li> <FontAwesomeIcon icon={ faGreaterThan } /> Mission Statements </li>
                         </ul>
                 </div>
 
                 <div className="Services-link">
                     <h2> Our Services </h2>
                     <ul>
-                        <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> Data Entry & Digitization </li>
-                        <li> <FontAwesomeIcon icon={ faGreaterThan } style={{color: "#000000",}} /> Bill Payment System </li>
+                        <li> <FontAwesomeIcon icon={ faGreaterThan } /> Data Entry & Digitization </li>
+                        <li> <FontAwesomeIcon icon={ faGreaterThan } /> Bill Payment System </li>
                     </ul>
                 </div>
 
                 <div className="information-links">
                     <h2> Information </h2>
                     <ul>
-                        <li> <FontAwesomeIcon icon={ faPhone } style={{color: "#000000",}} /> +977 984-7395310 </li>
-                        <li> <FontAwesomeIcon icon={ faEnvelope } style={{color: "#000000",}} /> info@nepcert.com </li>
-                        <li> <FontAwesomeIcon icon={ faLocationDot } style={{color: "#000000",}} /> Kumaripati, Lalitpur </li>
+                        <li> <FontAwesomeIcon icon={ faPhone } /> +977 984-7395310 </li>
+                        <li> <FontAwesomeIcon icon={ faEnvelope } /> info@nepcert.com </li>
+                        <li> <FontAwesomeIcon icon={ faLocationDot } /> Kumaripati, Lalitpur </li>
                     </ul>
                 </div>
+            </div>
 
                 <div className="copyright">
                     <p> © Copyright NEP CERT Private Limited. All Rights Reserved </p>
                 </div>
-                
+
             </div>
         </footer>
     )
