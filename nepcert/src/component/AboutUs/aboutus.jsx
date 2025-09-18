2c297c19ada582b2d6aad152ce11ca50bccf2b14
AjoobKansakar <ajoob.sagar@gmail.com>
2025-09-18 14:39:36 +0545

    updated aboutus section to dynamic array object

--- nepcert/src/component/AboutUs/aboutus.jsx
+++ nepcert/src/component/AboutUs/aboutus.jsx
@@ -2,20 +2,41 @@ import React from "react";
 import './aboutus.css';
 import Aboutusimg from "../../assets/about_us_img.png";
 
+const AboutUsContent = [
+    {
+        id: 1,
+        type: "heading1",
+        content: " About us "
+    },
+    {
+        id: 2,
+        type: "heading2", 
+        content: " Smart solutions for growing businesses! "
+    },
+    {
+        id: 3, 
+        type: "paragraph",
+        content: "Founded in 2006, Nep Cert Private Limited sets out to be a Business Process Outsourcing (BPO) company with IT and software solutions has been carrying out collection, data entry, imaging and uploading of the paper-based forms/records stored at different business organizations, within a short period of its establishment Nep Cert Private Limited as helped majority of Banks, Financial Institution and Telecom Company. With help of Nep Cert Organizations achieve more from thier IT investments and solutions."
+    }
+];
+
 function Aboutus () {    
     return (
         <div className="web-wrapper">
             <div className="aboutus" id="AboutUs">
                 <div className="aboutus-content">
-                    <h1> About us </h1>
-                    <h2> Smart solutions for growing businesses! </h2>
-                    <p> Founded in 2006, Nep Cert Private Limited sets out to be a Business Process Outsourcing (BPO)
-                        company with IT and software solutions has been carrying out collection, data entry,
-                        imaging and uploading of the paper-based forms/records stored at different business organizations,
-                        within a short period of its establishment Nep Cert Private Limited as helped majority of Banks, 
-                        Financial Institution and Telecom Company. With help of Nep Cert Organizations achieve more from thier 
-                        IT investments and solutions.
-                    </p>
+                    {
+                        AboutUsContent.map((item) => {
+                            if (item.type === "heading1") {
+                                return <h1 key={item.id}>{item.content}</h1>;
+                            } else if (item.type === "heading2"){
+                                return <h2 key={item.id}>{item.content}</h2>;
+                            } else if (item.type === "paragraph") {
+                                return <p key={item.id}>{item.content}</p>;
+                            }
+                            return null;
+                        })
+                    }
                 </div>
 
                 <div className="aboutus-img">
