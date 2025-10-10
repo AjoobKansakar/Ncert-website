052f5939eabf9c3bff5cab5a5c8c2e076287e5f0
AjoobKansakar <ajoob.sagar@gmail.com>
2025-10-10 15:18:16 +0545

    services section connecting api completed

--- nepcert/src/component/OurServices/services.jsx
+++ nepcert/src/component/OurServices/services.jsx
@@ -5,7 +5,8 @@ import DataEntryIcon from '../../assets/DataEntry_icon.svg';
 import BillPayIcon from '../../assets/Bill_icon.svg';
 
 
-function Services () {
+function Services ({Data}) {
+    console.log("services", Data);
 
     const [ activeIndex, setActiveIndex] = useState(null);
     
@@ -13,20 +14,20 @@ function Services () {
         setActiveIndex (prevIndex => (prevIndex === id ? null : id));
     }; 
 
-    const Servicesitems = [
-        {
-            id: 1,
-            title: "Data Entry and digitization",
-            icon: DataEntryIcon,
-            description: " Nep Cert has been carrying out data entry and imaging service for various organizations with hardware and man power for Global IME Bank, Laxmi Sunrise Bank Limited, American Life Insurance Company."
-        },
-        {
-            id: 2,
-            title: "Bill Payment System",
-            icon: BillPayIcon,
-            description: " Nep Cert Private Limited has managed to maintain the billing system for Nepal Telecom for past 9 years. A group of Staffs are dedicated for all the electricity bill generated for the Mobile BTS tower of Bagmati zone and periphery including remote regions are managed, paid and recorded in the Bill Payment System developed."
-        }
-    ];
+    // const Servicesitems = [
+    //     {
+    //         id: 1,
+    //         title: "Data Entry and digitization",
+    //         icon: DataEntryIcon,
+    //         description: " Nep Cert has been carrying out data entry and imaging service for various organizations with hardware and man power for Global IME Bank, Laxmi Sunrise Bank Limited, American Life Insurance Company."
+    //     },
+    //     {
+    //         id: 2,
+    //         title: "Bill Payment System",
+    //         icon: BillPayIcon,
+    //         description: " Nep Cert Private Limited has managed to maintain the billing system for Nepal Telecom for past 9 years. A group of Staffs are dedicated for all the electricity bill generated for the Mobile BTS tower of Bagmati zone and periphery including remote regions are managed, paid and recorded in the Bill Payment System developed."
+    //     }
+    // ];
 
     return (
 
@@ -41,7 +42,7 @@ function Services () {
 
                     <div className="services-present">
                         {/* mapping for flip card effect */}
-                        {Servicesitems.map(service => (
+                        {Data?.map(service => (
                             // card onclick flip feature
                             <div
                                 key={service.id}
@@ -49,11 +50,12 @@ function Services () {
                                 onClick={() => handleCardClick(service.id)}
                             >
                                 <div className="service-card-front"> 
-                                    <img src={service.icon} alt={`${service.title} icon`} className='service-icon'></img>
+                                    <img src={service.image} alt={`${service.title} icon`} className='service-icon'></img>
                                     <h2> {service.title} </h2>
                                 </div>
                                 <div className="service-card-description"> 
-                                    <p> {service.description} </p>
+                                    {/* dangerouslySetInnerHTML used to remove **<p></p>** from the box */}
+                                    <p dangerouslySetInnerHTML={{ __html: service.description }} />
                                 </div>
                             </div>
                         ))}
