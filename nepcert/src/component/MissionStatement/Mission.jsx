06387b205f40b5f70a16d9cd8052f8002fa6988d
AjoobKansakar <ajoob.sagar@gmail.com>
2025-10-14 22:37:15 +0545

    unwanted code commented out

--- nepcert/src/component/MissionStatement/Mission.jsx
+++ nepcert/src/component/MissionStatement/Mission.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Mission.css';
 import MissionBgImg from '../../assets/MissionStatement_img.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 // const missionPoints = [
 //     {
